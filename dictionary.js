661b63526df60ca3941e46153b697173240c6206
Carlos Eduardo Timaury Moreno <MINUTRADEBH\carlos.timaury@mt0032-ubuntu.minutradebh.local>
2016-09-20 11:18:28 -0300

    first commit

--- dictionary.js
+++ dictionary.js
@@ -3,9 +3,9 @@ var dictionary = {
 	'str_doNothing':'Nothing by the moment',
 	'str_doNothingResponse':'Its ok, no problem :)',
 	'str_getTracking':'Track a courier',
-	'str_chooseCarrier':'Please, choose a courier',
+	'str_chooseCarrier':'Please, choose a carrier',
 	'str_carrierFedex':'Fedex',
-	'str_carrierDHL':'DHL',
+	'str_carrierUPS':'UPS',
 	'str_carrierTNT':'TNT',
 	'str_askTrackingNumber':'Please write the trakking number (Remember to use no spaces between the digits)',
 }
