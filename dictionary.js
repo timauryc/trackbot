ad98821abb7a5d728c4e417c006ff506fdf85978
Carlos Eduardo Timaury Moreno <MINUTRADEBH\carlos.timaury@mt0032-ubuntu.minutradebh.local>
2016-09-21 17:07:15 -0300

    changes asked

--- dictionary.js
+++ dictionary.js
@@ -1,6 +1,6 @@
 var dictionary = {
-	'str_useInvitationBegin':'Hi, I\'m trackbot. I can help you track your packages. Please select your courier',
-	'str_useInvitationRepeat':'Would you like to track another package? If so, please select your courier',
+	'str_useInvitationBegin':'Hi, I\'m trackbot. I can help you track your packages. Please select your courier:',
+	'str_useInvitationRepeat':'Would you like to track another package? If so, please select your courier:',
 	'str_doNothing':'Nothing',
 	'str_doNothingResponse':'Its ok, no problem :)',
 	'str_getTracking':'Track a courier',
@@ -9,7 +9,7 @@ var dictionary = {
 	'str_carrierUPS':'UPS',
 	'str_carrierDHL':'DHL',
 	'str_askTrackingNumber':'Please write the trakking number (Remember to use no spaces between the digits)',
-	'str_trackingNotFound':'No tracking information was found, please verify the information you gave me and try again.',
+	'str_trackingNotFound':'No tracking information was found, please verify the information you gave me.',
 }
 
 module.exports = {
